4a6dd255c470c9b9c09656f7dd32db794ad73167
scottyTwoskip <scottyeansor@gmail.com>
2023-12-12 13:11:28 -0700

    did it work?

---
+++ petfax/pet.py
@@ -0,0 +1,22 @@
+from flask import (Blueprint, render_template)
+import json
+
+pets = json.load(open('pets.json'))
+print (pets)
+
+bp = Blueprint('pet', __name__, url_prefix="/pets")
+
+@bp.route('/')
+def index(): 
+    return render_template('index.html', pets=pets)
+
+@bp.route('/<int:index>')
+def show(index):
+    if index >= len(pets) or index < 0:
+        return "Pet not found", 404
+    pet = pets[index]
+    return render_template('show_pet.html', pet=pet)
+
+@bp.route('/facts/new')
+def new_fact():
+    return render_template('new_fact.html')
